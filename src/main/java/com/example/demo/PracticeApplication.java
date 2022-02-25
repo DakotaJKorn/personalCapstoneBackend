ad4d69fa53e09554009523cf01c373613098aec4
Dakota Korn <DakotaJKorn@gmail.com>
2022-02-25 14:56:38 -0500

    commit

--- src/main/java/com/example/demo/PracticeApplication.java
+++ src/main/java/com/example/demo/PracticeApplication.java
@@ -22,6 +22,7 @@ public class PracticeApplication {
 				registry.addMapping("/**")
 						.allowedMethods("*")
 						.allowedOrigins("https://finalprojectdakota.web.app")
+						.allowedOrigins("https://localhost:4200")
 						.allowedHeaders("*")
 						.allowCredentials(false)
 						.maxAge(-1);
